94705658a1e332fbe773c2892ad418859e725c5a
Akulij <aakulij@gmail.com>
2025-03-25 11:30:06 +0800

    add comment info about how to use admin password

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 
 type Config struct {
     BotToken string `env:"BOTTOKEN, required"`
-    AdminPass string `env:"ADMINPASSWORD, required"`
+    AdminPass string `env:"ADMINPASSWORD, required"` // to activate admin privileges in bot type command: /secret `AdminPass`
 }
 
 func GetConfig() Config {
