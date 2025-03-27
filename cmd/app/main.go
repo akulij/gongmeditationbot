b94873b6a80fd128430aad13c95a06d4d17676db
Akulij <aakulij@gmail.com>
2025-03-27 22:43:21 +0800

    move out assets for admin to panel file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -21,17 +21,6 @@ var adminCommands = map[string]func(BotController, tgbotapi.Update, User){
 	"/setchannelid": handleDefaultMessage, // just type it in channel which one is supposed to be lined with bot
 }
 
-var assets = map[string]string {
-    "Стартовая картинка":             "preview_image",
-    "Приветственный текст":           "start",
-    "Кнопка для заявки":              "leave_ticket_button",
-    "ID чата":                        "supportchatid",
-    "ID канала":                      "channelid",
-    "Уведомление об отправке тикета": "sended_notify",
-    "Просьба оставить тикет":         "leaveticket_message",
-    "Просьба подписаться на канал":   "subscribe_message",
-    "Ссылка на канал":                "channel_link",
-}
 
 func main() {
 	var bc = GetBotController()
---
+++ cmd/app/panel.go
@@ -0,0 +1,13 @@
+package main
+
+var assets = map[string]string {
+    "Стартовая картинка":             "preview_image",
+    "Приветственный текст":           "start",
+    "Кнопка для заявки":              "leave_ticket_button",
+    "ID чата":                        "supportchatid",
+    "ID канала":                      "channelid",
+    "Уведомление об отправке тикета": "sended_notify",
+    "Просьба оставить тикет":         "leaveticket_message",
+    "Просьба подписаться на канал":   "subscribe_message",
+    "Ссылка на канал":                "channel_link",
+}
