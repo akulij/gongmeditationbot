319a4b0f664ca08342d4cb905fcaec0d583ab4de
Akulij <aakulij@gmail.com>
2025-03-29 19:12:29 +0800

    add SheetID to config structure

--- config/config.go
+++ config/config.go
@@ -11,6 +11,7 @@ type Config struct {
     BotToken  string `env:"BOTTOKEN, required"`
     AdminPass string `env:"ADMINPASSWORD, required"` // to activate admin privileges in bot type command: /secret `AdminPass`
     AdminID   *int64 `env:"ADMINID"` // optional admin ID for notifications
+    SheetID   string `env:"SHEETID, required"` // id of google sheet where users will be synced
 }
 
 func GetConfig() Config {
